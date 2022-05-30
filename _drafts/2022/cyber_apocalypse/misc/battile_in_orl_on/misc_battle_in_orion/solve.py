b89f6dd56dc5d1a703193062680028215880921c
deut-erium <himanshuthesheoran@gmail.com>
2022-05-30 09:20:47 +0530

    battle on orion

--- _drafts/2022/cyber_apocalypse/misc/battile_in_orl_on/misc_battle_in_orion/solve.py
+++ _drafts/2022/cyber_apocalypse/misc/battile_in_orl_on/misc_battle_in_orion/solve.py
@@ -1,11 +1,23 @@
 from net import *
 import numpy as np
+import scipy.optimize as opt
 
 model = torch.load("model.pth",map_location=torch.device("cpu"))
 
 def save_np(h,w):
     with open(f'test_{h}x{w}.npy','wb') as f:
         np.save(f,np.ones((h,w)))
+
+TARGET = np.array([0.0470, 0.9530])
+
+def objective(np_arr):
+    target = TARGET
+    np_arr = np_arr.astype('float32')
+    value = model.forward(torch.from_numpy(np_arr.reshape(1,1,224,224)))
+    value = value.detach().numpy()[0]
+    return np.linalg.norm(target-value)
+
+x_init = np.zeros(224*224,dtype='float32')
 
 with open('final.npy', 'wb') as f:
     np.save(f, -6.943*np.ones((224,224)))
